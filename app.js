8b1a89cfd03084ae39823c1ded82dcd223749fa9
Dhumil Parekh <dhumilparekh@hotmail.com>
2021-06-29 16:02:23 -0500

    VueJs code added

---
+++ app.js
@@ -0,0 +1,10 @@
+const App = Vue.createApp({
+    // methods and content go here
+    data(){
+        return {firstName : "Dhumil",
+                lastName : "Parekh"
+    };
+    }
+});
+// Registering and mounting our app
+App.mount('#app');
